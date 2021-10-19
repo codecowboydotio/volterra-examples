ead046e804355e90211558e19e0c2a7ee728bacd
codecowboydotio <scott@vankalken.org>
2021-10-19 16:04:32 +1100

    updates to manifests and vars

--- single-site-single-app/vars.tf
+++ single-site-single-app/vars.tf
@@ -1,9 +1,9 @@
 variable "ns" { default = "s-vankalken" }
 #variable "domain" { default = "sa.f5demos.com" }
 variable "domain" { default = "sales-public.f5demos.com" }
-variable "servicename" { default = "svk-swapi-api2" }
+variable "servicename" { default = "svk-swapi-api" }
 
-variable "manifest_app_name" { default = "svk-swapi-api2" }
-variable "loadgen_manifest_app_name" { default = "svk-swapi-loadgen2" }
+variable "manifest_app_name" { default = "svk-swapi-api" }
+variable "loadgen_manifest_app_name" { default = "svk-swapi-loadgen" }
 
 variable "site_selector" { default = [ "ves.io/siteName in (ves-io-sg3-sin, ves-io-ny8-nyc, ves-io-os1-osa)" ] }
